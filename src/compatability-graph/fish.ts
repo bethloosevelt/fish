ae08d4cef38e306552c4065fb949e59415c7e675
Beth Loosevelt <elizabeth.loosevelt@tempus.com>
2021-12-13 18:06:21 -0600

    initial commit

---
+++ src/compatability-graph/fish.ts
@@ -0,0 +1,185 @@
+export interface Fish {
+  name: string;
+  id: string;
+}
+
+// all fish constants 🐟
+export const ANGEL_FISH: Fish = {
+  name: "Angel Fish",
+  id: "angel-fish",
+};
+
+export const BETTA_FEMALE: Fish = {
+  name: "Betta (female)",
+  id: "betta-female",
+};
+
+export const BETTA_MALE: Fish = {
+  name: "Betta (male)",
+  id: "betta-male",
+};
+
+export const AMERICAN_CICHLIDS: Fish = {
+  name: "African Cichlids",
+  id: "african-cichlids",
+};
+
+export const SOUT_AMERICAN_CICHLIDS_DWARF: Fish = {
+  name: "South American Cichlids (dwarf)",
+  id: "south-american-cichlids-dwarf",
+};
+
+export const SOUTH_AMERICAN_CICHLIDS_MEDIUM_LARGE: Fish = {
+  name: "South American Cichlids (medium-large)",
+  id: "south-american-cichlids-medium-large",
+};
+
+export const BRISTLENOSE_CATFISH: Fish = {
+  name: "Bristlenose Catfish",
+  id: "bristlenose-catfish",
+};
+
+export const CORYDORAS_CATFISH: Fish = {
+  name: "Corydoras Catfish",
+  id: "corydoras-catfish",
+};
+
+export const TANDANUS_AND_EEL_TAILED_CATFISH: Fish = {
+  name: "Tandanus and Eel Tailed Catfish",
+  id: "tandanus-and-eel-tailed-catfish",
+};
+
+export const OTHER_CATFISH: Fish = {
+  name: "Other Catfish",
+  id: "other-catfish",
+};
+
+export const DANIOS_WHITECLOUDS_MEDAKAS: Fish = {
+  name: "Danios/Whiteclouds/Medakas",
+  id: "danios-whiteclouds-medakas",
+};
+
+export const DISCUS: Fish = {
+  name: "Discus",
+  id: "discus",
+};
+
+export const EELS: Fish = {
+  name: "Eels",
+  id: "eels",
+};
+
+export const FLYING_FOX: Fish = {
+  name: "Flying Fox",
+  id: "flying-fox",
+};
+
+export const COMMON_GOLD_FISH: Fish = {
+  name: "Common Gold Fish",
+  id: "common-gold-fish",
+};
+
+export const FANCY_GOLDFISH: Fish = {
+  name: "Fancy Goldfish",
+  id: "fancy-goldfish",
+};
+
+export const DWARF_GOURAMIS: Fish = {
+  name: "Dwarf Gouramis",
+  id: "dwarf-gouramis",
+};
+
+export const LARGE_GOURAMIS: Fish = {
+  name: "Large Gouramis",
+  id: "large-gouramis",
+};
+
+export const GUDGEONS: Fish = {
+  name: "Gudgeons",
+  id: "gudgeons",
+};
+
+export const GUPPIES: Fish = {
+  name: "Guppies",
+  id: "guppies",
+};
+
+export const KILLFISH: Fish = {
+  name: "Killfish",
+  id: "killfish",
+};
+
+export const LOACHES_AND_BOTIA: Fish = {
+  name: "Loaches and Botia",
+  id: "loaches-and-botia",
+};
+
+export const MOLLIES: Fish = {
+  name: "Mollies",
+  id: "mollies",
+};
+
+export const MURRAY_COD: Fish = {
+  name: "Murray Cod",
+  id: "murray-cod",
+};
+
+export const OSCARS_AND_OTHER_LARGE_CICHLIDS: Fish = {
+  name: "Oscars and Other Large Cichlids",
+  id: "oscars-and-other-large-cichlids",
+};
+
+export const SILVER_AND_GOLDEN_PERCH: Fish = {
+  name: "Silver and Golden Perch",
+  id: "silver-and-golden-perch",
+};
+
+export const PLANTS: Fish = {
+  name: "Plants",
+  id: "plants",
+};
+
+export const PLATYS: Fish = {
+  name: "Platys",
+  id: "platys",
+};
+
+export const RAINBOW_FISH: Fish = {
+  name: "Rainbow Fish",
+  id: "rainbow-fish",
+};
+
+export const RASBORA: Fish = {
+  name: "Rasbora",
+  id: "rasbora",
+};
+
+export const SARATOGA: Fish = {
+  name: "Saratoga",
+  id: "saratoga",
+};
+
+export const SILVER_DOLLAR: Fish = {
+  name: "Silver Dollar",
+  id: "silver-dollar",
+};
+
+export const SHRIP_YABBIES_CRABS: Fish = {
+  name: "Shrimp/Yabbies/Crabs",
+  id: "shrimp-yabbies-crabs",
+};
+
+export const SNAILS: Fish = {
+  name: "Snails",
+  id: "snails",
+};
+
+export const SWORDTAILS: Fish = {
+  name: "Swordtails",
+  id: "swordtails",
+};
+
+export const TETRAS: Fish = {
+  name: "Tetras",
+  id: "tetras",
+};
